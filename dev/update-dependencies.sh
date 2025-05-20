7ba64d088e9360f28a144983b5a647000dc6cd05
Will <author@will.party>
2025-05-20 14:11:00 -0700

    Add script to update Bzlmod dependencies

---
+++ dev/update-dependencies.sh
@@ -0,0 +1,77 @@
+# Update all Bazel and Rust dependencies in a `MODULE.bazel` file
+# based on information from Bazel Central Registry and crates.io,
+# respectively.
+#
+# Arguments:
+# - Path to the `buildozer` executable.
+#
+# Requires:
+# - curl
+# - tail
+# - jq
+
+# Format output only if stderr (2) is a terminal (-t).
+if [ -t 2 ]
+then
+  # https://en.wikipedia.org/wiki/ANSI_escape_code
+  reset='\033[0m' # No formatting.
+  bold='\033[1m'
+  red='\033[1;31m'
+  green='\033[1;32m'
+  blue='\033[1;34m'
+else
+  # Make them all empty (no formatting) if stderr is piped.
+  reset=''
+  bold=''
+  red=''
+  green=''
+  blue=''
+fi
+
+# Move to the top level of the Git Repo for this function.
+# The source repo becomes the working directory.
+# Source files can be mutated, in contrast to Bazel's usual hermeticity.
+# https://bazel.build/docs/user-manual#running-executables
+if [ -z "$BUILD_WORKSPACE_DIRECTORY" ]
+then
+  echo >&2 -e "${red}Error$reset Run me with ${bold}bazel run$reset"
+  exit 1
+fi
+pushd "$BUILD_WORKSPACE_DIRECTORY" > /dev/null
+
+buildozer="$1"
+
+# The following creates a Buildozer command file to run a batch of commands together,
+# storing the contents in a variable.
+# Start by reading the name and version of each `bazel_dep` in `MODULE.bazel`.
+bazel_updates="$("$buildozer" 'print name version' '//MODULE.bazel:%bazel_dep' |
+  while read line
+  do
+    # Break each line into two space-delimited parts.
+    [[ "$line" =~ ([^ ]+)\ ([^ ]+) ]] && (
+      name="${BASH_REMATCH[1]}"
+      current_version="${BASH_REMATCH[2]}"
+
+      # Buildozer prints `(missing)` if the `bazel_dep` has no version.
+      # It's an `extern/` dependency. Just skip these.
+      [[ "$current_version" == '(missing)' ]] && exit
+
+      # URL of metadata JSON file for this dependency in BCR.
+      metadata_url="https://raw.githubusercontent.com/bazelbuild/bazel-central-registry/main/modules/$name/metadata.json"
+      # Get the latest version from the registry.
+      latest_version="$(curl --silent "$metadata_url" | jq --raw-output '.versions | last')"
+
+      if [[ "$current_version" != "$latest_version" ]]
+      then
+        echo >&2 -e "$green$name$reset $red$current_version$reset → $blue$latest_version$reset"
+        echo "replace version $current_version $latest_version|//MODULE.bazel:$name"
+      fi
+    )
+  done
+)"
+
+# Run all updates in a single Buildozer command file.
+echo -e "$bazel_updates" | "$buildozer" -f -
+
+# Go back to the initial working directory, because why not.
+popd > /dev/null
